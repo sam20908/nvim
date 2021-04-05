64594698acbaff479386b07aa2f5655d028e6769
sam20908 <samestimable2016@gmail.com>
2021-04-04 17:18:12 -0700

    Fixed few typos in remaps

--- lua/packages/vimspector.lua
+++ lua/packages/vimspector.lua
@@ -2,7 +2,9 @@ api = require('api')
 
 api.set_var('vimspector_enable_mappings', 'VISUAL_STUDIO')
 
-local opts = { silent = true }
+local opts = { noremap = true, silent = true }
+local opts_loud = { silent = true }
 
-api.set_keymap('n', '<LEADER>vi', '<PLUG>VimspectorBalloonEval', opts)
-api.set_keymap('x', '<LEADER>vi', '<PLUG>VimspectorBalloonEval', opts)
+api.set_keymap('n', '<LEADER>vr', '<CMD>VimspectorReset<CR>', opts)
+api.set_keymap('n', '<LEADER>vi', '<PLUG>VimspectorBalloonEval', opts_loud)
+api.set_keymap('x', '<LEADER>vi', '<PLUG>VimspectorBalloonEval', opts_loud)
