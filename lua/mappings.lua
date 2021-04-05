0f47c3daacf0c137a592d1506433570242230c2a
sam20908 <samestimable2016@gmail.com>
2021-04-04 20:44:04 -0700

    Add tab keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,13 +2,14 @@ api = require('api')
 
 local opts = { noremap = true, silent = true }
 
--- Split panes
 api.set_keymap('n', '<LEADER>sv', '<CMD>vsplit<CR>', opts)
 api.set_keymap('n', '<LEADER>sh', '<CMD>split<CR>', opts)
 
--- Buffer control
 api.set_keymap('n', '<LEADER>bd', '<CMD>bdelete<CR>', opts)
 
--- Quickfix shortcuts
 api.set_keymap('n', '<LEADER>qo', '<CMD>copen<CR>', opts)
 api.set_keymap('n', '<LEADER>qc', '<CMD>cclose<CR>', opts)
+
+api.set_keymap('n', '<LEADER>tc', '<CMD>tabclose<CR>', opts)
+api.set_keymap('n', '[r', '<CMD>tabnext<CR>', opts)
+api.set_keymap('n', ']r', '<CMD>tabprevious<CR>', opts)
