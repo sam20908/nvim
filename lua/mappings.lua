4658d373cc1efa148374b75040d1bf677aa1ff2c
sam20908 <samestimable2016@gmail.com>
2021-04-05 08:24:15 -0700

    Add window navigation keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,8 @@ vim.api.nvim_set_keymap('n', '<LEADER>qc', '<CMD>cclose<CR>', opts)
 vim.api.nvim_set_keymap('n', '<LEADER>tc', '<CMD>tabclose<CR>', opts)
 vim.api.nvim_set_keymap('n', '[r', '<CMD>tabnext<CR>', opts)
 vim.api.nvim_set_keymap('n', ']r', '<CMD>tabprevious<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', opts)
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', opts)
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', opts)
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', opts)
