9e8170139ca0e55ee5697930b57611f72cef2383
sam20908 <samestimable2016@gmail.com>
2021-04-13 21:20:38 -0700

    Fix cloning packer on Windows and reindent

--- lua/packages.lua
+++ lua/packages.lua
@@ -4,43 +4,43 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    execute 'packadd packer.nvim'
 end
 
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'lifepillar/vim-gruvbox8'
-  use 'edkolev/tmuxline.vim'
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+    use 'lifepillar/vim-gruvbox8'
+    use 'edkolev/tmuxline.vim'
 
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'preservim/nerdcommenter'
-  use 'neovim/nvim-lspconfig'
-  use 'onsails/lspkind-nvim'
-  use 'glepnir/lspsaga.nvim'
-  use 'hrsh7th/nvim-compe'
-  use 'cohama/lexima.vim'
-  use 'm-pilia/vim-ccls'
-  use 'puremourning/vimspector'
-  use { 'ilyachur/cmake4vim', branch = 'master' }
-  use 'mhinz/vim-startify'
-  use 'tpope/vim-dispatch'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'preservim/nerdcommenter'
+    use 'neovim/nvim-lspconfig'
+    use 'onsails/lspkind-nvim'
+    use 'glepnir/lspsaga.nvim'
+    use 'hrsh7th/nvim-compe'
+    use 'cohama/lexima.vim'
+    use 'm-pilia/vim-ccls'
+    use 'puremourning/vimspector'
+    use { 'ilyachur/cmake4vim', branch = 'master' }
+    use 'mhinz/vim-startify'
+    use 'tpope/vim-dispatch'
 
-  use 'kyazdani42/nvim-tree.lua'
-  use 'kyazdani42/nvim-web-devicons'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons'
 
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-telescope/telescope.nvim'
 
-  use 'tpope/vim-unimpaired'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
+    use 'tpope/vim-unimpaired'
+    use 'tpope/vim-surround'
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
 
-  use { 'mg979/vim-visual-multi', branch = 'master' }
-  use 'wellle/targets.vim'
+    use { 'mg979/vim-visual-multi', branch = 'master' }
+    use 'wellle/targets.vim'
 end)
