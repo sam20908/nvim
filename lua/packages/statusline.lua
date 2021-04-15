64f9992aaf0665a27fea042053d02943fa3e032e
sam20908 <samestimable2016@gmail.com>
2021-04-14 19:11:40 -0700

    Partially implemented git status in statusline

--- lua/packages/statusline.lua
+++ lua/packages/statusline.lua
@@ -1,17 +1,19 @@
 local gl = require('galaxyline')
 local fileinfo = require('galaxyline.provider_fileinfo')
 local condition = require('galaxyline.condition')
+local vcs = require('galaxyline.provider_vcs')
 local gls = gl.section
 
 local theme = require('galaxyline.theme').default
 
 local my_colors = {
     black = '#000000',
-    white = '#FFFFFF'
+    white = '#FFFFFF',
+    grey = '#403e37'
 }
 
 local function wrap_in_spaces(str)
-    return ' ' .. str .. ' '
+    return string.format(' %s ', str)
 end
 
 gls.left[1] = {
@@ -69,11 +71,13 @@ gls.left[4] = {
     }
 }
 
-gls.right[1] = {
+gls.right[3] = {
     FileEncode = {
-        provider = 'FileEncode',
+        provider = function()
+            return wrap_in_spaces(fileinfo.get_file_encode())
+        end,
         condition = condition.buffer_not_empty,
-        highlight = { my_colors.white, theme.bg }
+        highlight = { my_colors.black, theme.green }
     }
 }
 
@@ -84,7 +88,27 @@ gls.right[2] = {
         end,
         condition = condition.buffer_not_empty,
         separator = ' ',
-        highlight = { my_colors.black, theme.green },
+        highlight = { my_colors.white, my_colors.grey },
         separator_highlight = { theme.bg, theme.bg }
+    }
+}
+
+gls.right[1] = {
+    VCS = {
+        --provider = { 'GitBranch', 'DiffAdd', 'DiffModified', 'DiffRemove' },
+        provider = function()
+            local branch = vcs.get_git_branch()
+            local adds = vcs.diff_add()
+            local modifies = vcs.diff_modified()
+            local removes = vcs.diff_remove()
+
+            adds = adds and adds or 0
+            modifies = modifies and modifies or 0
+            removes = removes and removes or 0
+
+            return string.format('%s +%d ~%d -%d', branch, adds, modifies, removes)
+        end,
+        condition = condition.check_git_workspace,
+        highlight = { my_colors.white, theme.bg }
     }
 }
