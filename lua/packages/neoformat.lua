877f4a3e44099bbf4c2fa62155171fb8ef9e328e
Sam Huang <1443830@sd33online.ca>
2022-05-26 09:30:01 -0700

    Add rustfmt as Rust formatter

--- lua/packages/neoformat.lua
+++ lua/packages/neoformat.lua
@@ -2,6 +2,7 @@ vim.g.neoformat_cpp_clangformat = { exe = 'clang-format' }
 
 vim.g.neoformat_enabled_cpp = {'clangformat'}
 vim.g.neoformat_enabled_c = {'clangformat'}
+vim.g.neoformat_enabled_rust = {'rustfmt'}
 
 local opts = { noremap = true, silent = true }
 
